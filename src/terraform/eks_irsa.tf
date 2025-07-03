e0cc04077621b952bf559b7e6a121c3e25dcd0e1
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-04 00:47:10 +0200

    implement irsa

--- src/terraform/eks_irsa.tf
+++ src/terraform/eks_irsa.tf
@@ -3,3 +3,28 @@ resource "aws_iam_openid_connect_provider" "eks_oidc" {
     thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
     url = aws_eks_cluster.eks_dev_cluster.identity[0].oidc[0].issuer
 }
+
+resource "aws_iam_role" "eks_irsa_iam_role" {
+    name = "eks_irsa_iam_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Action = "sts:AssumeRoleWithWebIdentity"
+            Principal = {
+                Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+            }
+            Condition = {
+                StringEquals = {
+                    "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:default:irsa-demo-sa"
+
+                }
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_irsa_iam_role_policy" {
+    role = aws_iam_role.eks_irsa_iam_role.arn
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
