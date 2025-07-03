232fcbc6f8a2cb7e1fb73f8ad54431dccdefd441
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-04 00:07:09 +0200

    oidc provider arn

---
+++ src/terraform/outputs.tf
@@ -0,0 +1,8 @@
+locals {
+    aws_iam_openid_connect_provider_extract_from_arn = element(split("oidc-provider/", aws_iam_openid_connect_provider.eks_oidc.arn))
+}
+
+output "aws_iam_openid_connect_provider_arn" {
+    description = "AWS IAM OIDC provider ARN"
+    value = local.aws_iam_openid_connect_provider_extract_from_arn
+}
