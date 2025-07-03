eb0d318bf6dc075b98cf1f32a261e6ba80d7a42b
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-04 00:12:18 +0200

    fix typo

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,5 +1,5 @@
 locals {
-    aws_iam_openid_connect_provider_extract_from_arn = element(split("oidc-provider/", aws_iam_openid_connect_provider.eks_oidc.arn))
+    aws_iam_openid_connect_provider_extract_from_arn = element(split("oidc-provider/", aws_iam_openid_connect_provider.eks_oidc.arn), 1)
 }
 
 output "aws_iam_openid_connect_provider_arn" {
