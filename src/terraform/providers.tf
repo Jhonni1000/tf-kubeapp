f2a7d1873690133550889ee0693d2635d3ac66a9
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-08 15:37:41 +0200

    remove aws-auth provision

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -15,16 +15,6 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-data "aws_eks_cluster_auth" "my_cluster" {
-  name = aws_eks_cluster.eks_dev_cluster.name
-}
-
-provider "kubernetes" {
-  host = aws_eks_cluster.eks_dev_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_dev_cluster.certificate_authority[0].data)
-  token = data.aws_eks_cluster_auth.my_cluster.token
-}
-
 terraform {
   backend "s3" {
     bucket = "tf-statefile-19999"
