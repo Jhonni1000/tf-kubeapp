bf1e387f1536451a36d6ab15a122054a31f9d353
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-09 01:09:21 +0200

    new

--- src/terraform/eks.tf
+++ src/terraform/eks.tf
@@ -53,6 +53,7 @@ resource "aws_iam_role_policy_attachment" "eks_nodegroup_role_policy" {
 resource "aws_eks_cluster" "eks_dev_cluster" {
   name     = "eks_dev_cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.31"
 
   vpc_config {
     subnet_ids              = module.vpc.public_subnets
