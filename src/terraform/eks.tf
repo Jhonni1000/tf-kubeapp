f70aea0ac2b873900e98b963a559162086134c0c
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-09 13:35:27 +0200

    new

--- src/terraform/eks.tf
+++ src/terraform/eks.tf
@@ -92,9 +92,9 @@ resource "aws_eks_node_group" "eks_dev_ng" {
   }
 
   scaling_config {
-    desired_size = 5
+    desired_size = 3
     min_size     = 2
-    max_size     = 7
+    max_size     = 5
   }
 
   update_config {
