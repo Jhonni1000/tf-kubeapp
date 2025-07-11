e5bc1cddaa50a92b4218ad1e802c57776665982a
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-07-11 09:37:41 +0200

    test

--- src/terraform/eks.tf
+++ src/terraform/eks.tf
@@ -43,7 +43,8 @@ resource "aws_iam_role_policy_attachment" "eks_nodegroup_role_policy" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AutoScalingFullAccess"
   ])
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = each.value
@@ -75,7 +76,52 @@ resource "aws_eks_cluster" "eks_dev_cluster" {
   ]
 }
 
-resource "aws_eks_node_group" "eks_dev_ng" {
+
+resource "aws_eks_node_group" "eks_public_dev_ng" {
+  cluster_name = aws_eks_cluster.eks_dev_cluster.name
+
+  node_group_name = "eks_dev_ng"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = module.vpc.public_subnets
+
+  ami_type       = "AL2_x86_64"
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+  instance_types = ["t3.medium"]
+
+  remote_access {
+    ec2_ssh_key = "testkey"
+  }
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = {
+    tags = {
+    Environment = "dev"
+    Managed_by  = "OPAKI"
+    Name = "Public-Node-Group"
+    "k8s.io/cluster-autoscaler/${aws_eks_cluster.eks_dev_cluster.name}" = "owned"
+    "k8s.io/cluster-autoscaler/enabled" = "true"
+  }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_nodegroup_role_policy,
+    aws_eks_cluster.eks_dev_cluster
+  ]
+}
+
+
+/*
+resource "aws_eks_node_group" "eks_private_dev_ng" {
   cluster_name = aws_eks_cluster.eks_dev_cluster.name
 
   node_group_name = "eks_dev_ng"
@@ -92,9 +138,9 @@ resource "aws_eks_node_group" "eks_dev_ng" {
   }
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     min_size     = 1
-    max_size     = 3
+    max_size     = 2
   }
 
   update_config {
@@ -104,6 +150,9 @@ resource "aws_eks_node_group" "eks_dev_ng" {
   tags = {
     Environment = "dev"
     Managed_by  = "OPAKI"
+    Name = "Private-Node-Group"
+    "k8s.io/cluster-autoscaler/${aws_eks_cluster.eks_dev_cluster.name}" = "owned"
+    "k8s.io/cluster-autoscaler/enabled" = "TRUE"
   }
 
   depends_on = [
@@ -111,3 +160,4 @@ resource "aws_eks_node_group" "eks_dev_ng" {
     aws_eks_cluster.eks_dev_cluster
   ]
 }
+*/
